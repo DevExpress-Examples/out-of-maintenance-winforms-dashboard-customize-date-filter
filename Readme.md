a661ddfbdcd2dd3317538437d396f1e1aa808cde
BrianDX <boris.zaitsev@devexpress.com>
2019-04-24 18:35:41 +0300

    example content

---
+++ Readme.md
@@ -0,0 +1,36 @@
+<!-- default file list -->
+*Files to look at*:
+
+* [Form1.cs](./CS/DateFilter_Customization/Form1.cs) (VB: [Form1.vb](./VB/DateFilter_Customization/Form1.vb))
+<!-- default file list end -->
+
+# How to Customize the Date Filter Dashboard Item
+
+This example demonstrates how to access the underlying controls to customize the [Date Filter](https://docs.devexpress.com/Dashboard/400675) dashboard item.
+
+It changes the datepicker's caption, background color and paints selected dates and dates contained in the underlying data in a custom manner.
+
+![screenshot](images/screenshot.png)
+
+API in this example:
+
+* [DashboardViewer.DashboardItemControlCreated](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWin.DashboardViewer.DashboardItemControlCreated) event
+* [DashboardViewer.DashboardItemControlUpdated](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWin.DashboardViewer.DashboardItemControlUpdated) event
+* [DashboardViewer.DashboardItemBeforeControlDisposed](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWin.DashboardViewer.DashboardItemBeforeControlDisposed) event
+* [DashboardItemControlEventArgs.DateFilterControl](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWin.DashboardItemControlEventArgs.DateFilterControl) property
+* [DateFilterControl](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWin.DateFilterControl) class
+* [DateFilterControl.CalendarFrom](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWin.DateFilterControl.CalendarFrom) property
+* [DateFilterControl.CalendarTo](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWin.DateFilterControl.CalendarTo) property
+* [CalendarControl](https://docs.devexpress.com/WindowsForms/DevExpress.XtraEditors.Controls.CalendarControl) class
+* [CalendarControl.CustomDrawDayNumberCell](https://docs.devexpress.com/WindowsForms/DevExpress.XtraEditors.Controls.CalendarControlBase.CustomDrawDayNumberCell) event
+* [DateFilterControl.CustomDisplayText](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWin.DateFilterControl.CustomDisplayText) event
+* [DashboardViewer.GetItemData](https://docs.devexpress.com/Dashboard/DevExpress.DashboardWin.DashboardViewer.GetItemData(System.String)) method
+* [MultiDimensionalData](https://docs.devexpress.com/Dashboard/DevExpress.DashboardCommon.ViewerData.MultiDimensionalData) class
+* [MultiDimensionalData.GetMeasures](https://docs.devexpress.com/Dashboard/DevExpress.DashboardCommon.ViewerData.MultiDimensionalData.GetMeasures) method
+
+
+
+
+**See also:**
+
+* [Access to Underlying Controls](https://docs.devexpress.com/Dashboard/18019)
---
+++ VB/DateFilter_Customization/Form1.vb
@@ -0,0 +1,95 @@
+﻿Imports DevExpress.DashboardCommon.ViewerData
+Imports DevExpress.DashboardWin
+Imports DevExpress.XtraEditors.Controls
+Imports System
+Imports System.Collections.Generic
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+
+Namespace DateFilter_Customization
+	Partial Public Class Form1
+		Inherits DevExpress.XtraEditors.XtraForm
+
+		Private minValue As Date
+		Private maxValue As Date
+		Public Sub New()
+			InitializeComponent()
+			AddHandler Me.Load, AddressOf Form1_Load
+			AddHandler dashboardViewer1.DashboardItemControlCreated, AddressOf DashboardViewer1_DashboardItemControlCreated
+			AddHandler dashboardViewer1.DashboardItemControlUpdated, AddressOf DashboardViewer1_DashboardItemControlUpdated
+			AddHandler dashboardViewer1.DashboardItemBeforeControlDisposed, AddressOf DashboardViewer1_DashboardItemBeforeControlDisposed
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+			dashboardViewer1.LoadDashboard("Data\datefilter-sample-dashboard.xml")
+		End Sub
+
+
+		Private Sub DashboardViewer1_DashboardItemControlCreated(ByVal sender As Object, ByVal e As DevExpress.DashboardWin.DashboardItemControlEventArgs)
+			Dim designer As DashboardViewer = TryCast(sender, DashboardViewer)
+
+			If e.DateFilterControl IsNot Nothing Then
+				SubscribeDateFilterControlEvents(e.DateFilterControl)
+				e.DateFilterControl.BackColor = Color.AliceBlue
+			End If
+		End Sub
+
+		Private Sub SubscribeDateFilterControlEvents(ByVal dateFilter As DateFilterControl)
+			AddHandler dateFilter.CalendarFrom.CustomDrawDayNumberCell, AddressOf Calendar_CustomDrawDayNumberCell
+			AddHandler dateFilter.CalendarTo.CustomDrawDayNumberCell, AddressOf Calendar_CustomDrawDayNumberCell
+			AddHandler dateFilter.CustomDisplayText, AddressOf DateFilter_CustomDisplayText
+		End Sub
+
+		Private Sub DateFilter_CustomDisplayText(ByVal sender As Object, ByVal e As CustomDisplayTextEventArgs)
+			e.DisplayText = If(TypeOf e.Value Is Date, String.Format("{0:d}", e.Value), "Click for the Date Picker")
+		End Sub
+
+		Private Sub Calendar_CustomDrawDayNumberCell(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Calendar.CustomDrawDayNumberCellEventArgs)
+			Dim calendar As CalendarControl = TryCast(sender, CalendarControl)
+
+			If e.Date > minValue AndAlso e.Date < maxValue Then
+				e.Style.BackColor = Color.FromArgb(80, 0, 100, 10)
+			End If
+
+
+			If e.Selected AndAlso e.View = DateEditCalendarViewType.MonthInfo Then
+				Dim dayFormat As New StringFormat()
+				dayFormat.Alignment = StringAlignment.Center
+				dayFormat.LineAlignment = StringAlignment.Center
+				Dim rect As Rectangle = e.ContentBounds
+				rect.Inflate(2, 2)
+				Dim cellFont As New Font(calendar.CalendarAppearance.DayCell.Font.FontFamily, calendar.CalendarAppearance.DayCell.Font.Size + 2)
+				e.Cache.FillRectangle(New SolidBrush(Color.Yellow), e.ContentBounds)
+				e.Cache.Graphics.DrawString($"{e.Date.Day}", cellFont, Brushes.Black, rect, dayFormat)
+				e.Handled = True
+			End If
+		End Sub
+
+		Private Sub DashboardViewer1_DashboardItemControlUpdated(ByVal sender As Object, ByVal e As DashboardItemControlEventArgs)
+			If e.DateFilterControl IsNot Nothing Then
+				UpdateDateFilterControl(e.DateFilterControl)
+			End If
+		End Sub
+
+		Private Sub UpdateDateFilterControl(ByVal dateFilter As DateFilterControl)
+			Dim mddata As MultiDimensionalData = dashboardViewer1.GetItemData("dateFilterDashboardItem1")
+			Dim values As IList(Of Date) = mddata.GetMeasures().Select(Function(measure) mddata.GetValue(measure).Value).OfType(Of Date)().ToList()
+			minValue = values.Min()
+			maxValue = values.Max()
+		End Sub
+
+		Private Sub UnsubscribeDateFilterControlEvents(ByVal dateFilter As DateFilterControl)
+			Dim calendarTo As CalendarControl = dateFilter.CalendarTo
+			Dim calendarFrom As CalendarControl = dateFilter.CalendarFrom
+			RemoveHandler calendarFrom.CustomDrawDayNumberCell, AddressOf Calendar_CustomDrawDayNumberCell
+			RemoveHandler calendarTo.CustomDrawDayNumberCell, AddressOf Calendar_CustomDrawDayNumberCell
+		End Sub
+
+		Private Sub DashboardViewer1_DashboardItemBeforeControlDisposed(ByVal sender As Object, ByVal e As DashboardItemControlEventArgs)
+			If e.DateFilterControl IsNot Nothing Then
+				UnsubscribeDateFilterControlEvents(e.DateFilterControl)
+			End If
+		End Sub
+	End Class
+End Namespace
